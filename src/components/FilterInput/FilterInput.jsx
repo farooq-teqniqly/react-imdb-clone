e03109a761f51e014cbfcb2454b0193faa82825b
farooq-teqniqly <farooq@teqniqly.com>
2025-09-28 16:10:47 -0700

    style: Autofocus input in FilterInput component using useRef and useEffect

--- src/components/FilterInput/FilterInput.jsx
+++ src/components/FilterInput/FilterInput.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useRef } from "react";
+
 export const FilterInput = ({ filter, onFilterChanged }) => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
   return (
     <div>
       <input
+        ref={inputRef}
         type="text"
         className="form-control"
         style={{ width: "300px" }}
