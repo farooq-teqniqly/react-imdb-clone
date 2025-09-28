5eb85eae69eb3207ea2df7e426f433248f95e968
farooq-teqniqly <farooq@teqniqly.com>
2025-09-28 16:05:00 -0700

    style: Add styling to FilterInput component for improved input appearance

--- src/components/FilterInput/FilterInput.jsx
+++ src/components/FilterInput/FilterInput.jsx
@@ -3,6 +3,8 @@ export const FilterInput = ({ filter, onFilterChanged }) => {
     <div>
       <input
         type="text"
+        className="form-control"
+        style={{ width: "300px" }}
         value={filter}
         placeholder="Filter coins by name or symbol"
         onChange={(e) => onFilterChanged(e.target.value)}
