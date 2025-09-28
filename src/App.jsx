3f9c360f95efe48e70d55515fcf19101ffd2a661
farooq-teqniqly <farooq@teqniqly.com>
2025-09-28 14:41:14 -0700

    feat: Add Crypto Dashboard with CoinGecko API integration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,79 @@
-import { HelloBootstrap } from "./pages/HelloBootstrap";
+import { useState, useEffect } from "react";
+import { Container, Col, Row, Card, Image } from "react-bootstrap";
+
+const API_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`;
 
 function App() {
-  return <HelloBootstrap></HelloBootstrap>;
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
+
+        if (!res.ok) {
+          const error = `Failed to fetch data: ${res.status} ${res.statusText}`;
+          console.error(error);
+          setError(error);
+          return;
+        }
+
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+  }, []);
+
+  const getPercentChangeClass = (coin) => {
+    return coin.price_change_percentage_24h < 0
+      ? "text-danger"
+      : "text-success";
+  };
+
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+      <Container className="my-5 mx-0">
+        {loading && <p>Loading...</p>}
+        {error && <div>{error}</div>}
+
+        {!loading && !error && (
+          <Row>
+            {coins.map((coin) => (
+              <Col md={3} className="mb-5">
+                <Card>
+                  <Card.Header>
+                    <Image src={coin.image} alt={coin.name} thumbnail />
+                    {coin.name}{" "}
+                    <p className="fw-lighter text-uppercase">({coin.symbol})</p>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Text>
+                      <p>Price: ${coin.current_price.toLocaleString()}</p>
+                      <p className={getPercentChangeClass(coin)}>
+                        {coin.price_change_percentage_24h.toFixed(2)}%
+                      </p>
+                      <p>Market Cap: ${coin.market_cap.toLocaleString()}</p>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </Container>
+    </div>
+  );
 }
 
 export default App;
