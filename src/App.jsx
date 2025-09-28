d401a82993286ff4a4b792ab48144a79d3ba161c
farooq-teqniqly <farooq@teqniqly.com>
2025-09-28 15:17:20 -0700

    chore: Update API URL to use dynamic base URL and add example environment variable for API base URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Col, Row, Card, Image } from "react-bootstrap";
 
-const API_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`;
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}&x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`;
 
 function App() {
   const [coins, setCoins] = useState([]);
